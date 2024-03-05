e5a01ca5ee008643f3f2ea5b43a85e76e431bade
a.polyakov <avpgenium@yandex.ru>
2024-03-05 12:05:52 +0300

    add map-reduce task1

--- map_reduce/task1/task2Reducer.py
+++ map_reduce/task1/task2Reducer.py
@@ -1,5 +1,27 @@
+#! /usr/bin/env python3
 import sys
 
+max_value = 0
+max_year = None
+max_month = None
+min_value = 1000
+min_year = None
+min_month = None
+
 for line in sys.stdin:
-    # max_yaer  max_month   max_value
-    # min_yaer  min_month   min_value
+    year, value = line.strip().split('\t')
+    count, month = value.strip().split(',')
+    count = int(count)
+
+    if max_value < count:
+        max_value = count
+        max_year = year
+        max_month = month
+
+    if min_value > count:
+        min_value = count
+        min_year = year
+        min_month = month
+
+print(str(max_year) + '\t' + str(max_month) + '\t' + str(max_value))
+print(str(min_year) + '\t' + str(min_month) + '\t' + str(min_value))
