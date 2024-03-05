5a6e0606aa458f25f517d7d8093fd55644f3d826
a.polyakov <avpgenium@yandex.ru>
2024-03-05 11:36:17 +0300

    add map-reduce task1

---
+++ map_reduce/task1/task1Mapper.py
@@ -0,0 +1,9 @@
+import sys
+
+for line in sys.stdin:
+    inputData = line.strip().split('  ')
+    year = inputData[0]
+    data = inputData[1:-1]
+    for idx in range(len(data)):
+        value = data[idx]
+        print(str(year) + '\t' + value + ',' + str(idx))
---
+++ map_reduce/task1/task2Reducer.py
@@ -0,0 +1,5 @@
+import sys
+
+for line in sys.stdin:
+    # max_yaer  max_month   max_value
+    # min_yaer  min_month   min_value
