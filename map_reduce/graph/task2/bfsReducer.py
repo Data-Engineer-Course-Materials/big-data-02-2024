0127d7dbac7d1b8ddbb53c9ad6a480d37662346e
a.polyakov <avpgenium@yandex.ru>
2024-03-11 11:14:48 +0300

    add map-reduce tasks

---
+++ map_reduce/graph/task2/bfsReducer.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import sys
+
+dict_graph = {}
+
+for line in sys.stdin:
+    v_input, weight, adjustment_string = line.strip().split()
+    adjustment_list = adjustment_string[1:-1].strip().split(',')
+
+    if weight.strip() == 'INF':
+        weight = float('inf')
+    else:
+        weight = int(weight)
+
+    value = {}
+    value.update({'weight': weight})
+    value.update({'adjustment_list': adjustment_list})
+
+    if v_input not in dict_graph:
+        dict_graph[v_input] = value
+    else:
+        if weight < dict_graph[v_input]['weight']:
+            dict_graph[v_input]['weight'] = weight
+        dict_graph[v_input]['adjustment_list'].extend(adjustment_list)
+
+for key in dict_graph:
+    adjustment_list_result = []
+    for node in dict_graph[key]['adjustment_list']:
+        if node != '':
+            adjustment_list_result.append(node)
+    weight = str(dict_graph[key]['weight']).upper()
+    print(f"{key}\t{weight}\t" + '{' ','.join(adjustment_list_result) + '}')
