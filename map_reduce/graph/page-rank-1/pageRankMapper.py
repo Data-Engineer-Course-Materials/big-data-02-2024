e011677253e5f0668d8b808d8618dd466165dba1
a.polyakov <avpgenium@yandex.ru>
2024-03-12 10:09:13 +0300

    add map-reduce tasks

--- map_reduce/graph/page-rank-1/pageRankMapper.py
+++ map_reduce/graph/page-rank-1/pageRankMapper.py
@@ -4,9 +4,9 @@ import sys
 import re
 
 for line in sys.stdin:
-    print(line.strip())
-    page_rank = line.strip().split('\t')[1]
-    out_nodes = line.strip().split('\t')[2]
+    print(line.strip().replace('   ', '\t'))
+    page_rank = float(line.strip().split('   ')[1])
+    out_nodes = line.strip().split('   ')[2]
     list_out_nodes = re.sub("[{}]", "", out_nodes).split(",")
     p = float(round(page_rank / len(list_out_nodes), 3))
     p = "%.3f" % p
